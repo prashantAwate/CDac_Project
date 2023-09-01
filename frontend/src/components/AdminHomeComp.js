56909c9f7c2403296f3f6b25fa450e14caa9989b
TusharShingade <tush0417@gmail.com>
2023-09-01 15:08:01 +0530

    HomeComp Updated

--- frontend/src/components/AdminHomeComp.js
+++ frontend/src/components/AdminHomeComp.js
@@ -1,10 +1,13 @@
 import NavigationBarAdmin from "./NavigationBarAdmin"
+import image4 from "./images/img4.jpeg";
 export default function AdminHomeComp()
 {
     return(
         <div>
             <NavigationBarAdmin></NavigationBarAdmin>
-
+        
+            <div style={{ backgroundImage: `url(https://electronicsera.in/wp-content/uploads/2023/01/Latest-semiconductor-manufacturing-developments-in-India......jpg)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', minHeight: '93vh'  }}>
+            </div>
         </div>
         
     )
